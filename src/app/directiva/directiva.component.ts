352de53ac7560ab90da85bb70756cec42bca0121
kenydanfer01 <kenychumacero19@gmail.com>
2023-05-22 11:13:33 -0500

    Add *ngIf

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
 })
 export class DirectivaComponent {
   listaCurso: string[] = ['Typescript', 'JavaScript', 'Java SE', 'C#', 'PHP'];
+  habilitar: boolean = true;
   constructor() {}
+
+  setHabilitar(): void {
+    this.habilitar = this.habilitar == true ? false : true;
+  }
 }
